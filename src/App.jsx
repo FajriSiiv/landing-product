f021e48cc02c5c8084764a3349bcdbbf0cdc7476
Fajri Siiv <fajrijijin@gmail.com>
2023-02-15 23:30:22 +0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import MainContent from "./components/main-content";
+
+function App() {
+  return (
+    <div className="">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="flex justify-around items-center container pt-5 pb-3 fixed -translate-x-1/2  left-1/2">
+      <div>
+        <p className="text-3xl font-bold">Product.</p>
+      </div>
+      <div className="flex gap-10 text-sm">
+        <p className="font-light ">Home</p>
+        <p className="font-light ">Home</p>
+        <p className="font-light ">Home</p>
+        <p className="font-light ">Home</p>
+      </div>
+      <button className="py-3 px-6 bg-[#7214FF] rounded-full text-white text-sm">
+        Get a Demo
+      </button>
+    </div>
+  );
+}
