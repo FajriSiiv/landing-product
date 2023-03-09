7869b93c915703592c2049c8f9be675d764bb58e
Fajri Siiv <fajrijijin@gmail.com>
2023-03-09 11:42:02 +0700

    add analitys

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "./footer";
 import Header from "./header";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-[#060B27] w-full min-h-screen text-white sm:overflow-hidden">
       <Header />
       {children}
+      <Analytics />
       <Footer />
     </div>
   );
